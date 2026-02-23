4fcc065b7f19dd4bc21a85bc3dbb1ff8659741ec
ucanspamifuwant-png <ucanspam.ifuwant@gmail.com>
2026-02-23 15:59:14 +0100

    Fix mentions legales

--- frontend/src/pages/MentionsLegales.jsx
+++ frontend/src/pages/MentionsLegales.jsx
@@ -9,7 +9,7 @@ export function MentionsLegales() {
       <p>
         Nom du site : PC-ARCHI
         <br />
-        Responsable de publication : PC Archi (pseudonyme)
+        Responsable de publication : PC Archi (éditeur de site)
         <br />
         Contact :{" "}
         <a href="mailto:pcarchi.contact@gmail.com">pcarchi.contact@gmail.com</a>
