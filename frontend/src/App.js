c7c9cfdef4402b52ea064a87e95b34f876d22d9c
ucanspamifuwant-png <ucanspam.ifuwant@gmail.com>
2026-02-23 16:09:19 +0100

    Add contact route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { CategoryPage } from "./pages/CategoryPage";
 import { ArticlePage } from "./pages/ArticlePage";
+import { Contact } from "./pages/Contact";
 import { MentionsLegales } from "./pages/MentionsLegales";
 import { PolitiqueConfidentialite } from "./pages/PolitiqueConfidentialite";
 import { PolitiqueCookies } from "./pages/PolitiqueCookies";
@@ -15,6 +16,7 @@ function App() {
         <Route path="/categorie/:slug" element={<CategoryPage />} />
         <Route path="/article/:slug" element={<ArticlePage />} />
         <Route path="/mentions-legales" element={<MentionsLegales />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/politique-confidentialite" element={<PolitiqueConfidentialite />} />
         <Route path="/cookies" element={<PolitiqueCookies />} />
       </Routes>
